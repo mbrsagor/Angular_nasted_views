6925ac82001b25fda4e1491fd5c7eb972b1cb945
mbrsagor <mbrsagor@gmail.com>
2021-09-03 00:53:48 +0600

    New: Add template

--- myapp/settings.py
+++ myapp/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +64,7 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, os.path.join('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
