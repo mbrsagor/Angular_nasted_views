2ed157230c0c14b036078fbddf43494914df2ef0
mbrsagor <mbrsagor@gmail.com>
2021-09-05 22:29:33 +0600

    New: Artilce search page created

--- articles/urls.py
+++ articles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.articles_list_view, name='article'),
     path('article/<int:id>/', views.article_details, name='article_details'),
+    path('search/', views.article_search_view, name='article_search'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -12,9 +12,27 @@ def articles_list_view(request):
 
 
 def article_details(request, id):
-    article = Article.objects.get(id=id)
-    context = {
-        'article': article
-    }
-    template_name = 'article_details.html'
-    return render(request, template_name, context)
+    if id is not None:
+        article = Article.objects.get(id=id)
+        context = {
+            'article': article
+        }
+        template_name = 'article_details.html'
+        return render(request, template_name, context)
+    else:
+        return False
+
+
+def article_search_view(request):
+    # print(dir(request))
+    # print(request.GET)
+    query_dict = request.GET  # This is a dictionary
+    article_obj = None
+    try:
+        query = int(query_dict.get("q")) # <input type="text" name="q" />
+    except:
+        query = None
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+    template_name = 'search.html'
+    return render(request, template_name, {"object": article_obj})
