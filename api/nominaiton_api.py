d0e5e7ccfb5469c327f749b038686402d14e2513
mbrsagor <mbrsagor@gmail.com>
2021-11-30 23:33:18 +0600

    New: Symbol API endpoint created

--- api/nominaiton_api.py
+++ api/nominaiton_api.py
@@ -2,7 +2,30 @@ from rest_framework import serializers
 from rest_framework import views, status
 from rest_framework.response import Response
 
-from app.models.nomination import Nomination
+from app.models.nomination import Nomination, Symbol
+
+
+class SymbolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Symbol
+        fields = (
+            'id', 'name'
+        )
+
+
+class SymbolListAPIView(views.APIView):
+
+    def get(self, request):
+        symbol = Symbol.objects.all()
+        serializer = SymbolSerializer(symbol, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = SymbolSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class NominationSerializer(serializers.ModelSerializer):
