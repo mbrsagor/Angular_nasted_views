7de7d59eae0580885751bbc782429bfe3a3c0017
mbrsagor <mbrsagor@gmail.com>
2021-11-30 23:35:29 +0600

    Refactor: Symbol API url added

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.nominaiton_api import ListOfNominationView
+from api.nominaiton_api import SymbolListAPIView, ListOfNominationView
 
 urlpatterns = [
+    path('symbol/', SymbolListAPIView.as_view()),
     path('nominaiton', ListOfNominationView.as_view())
 ]
