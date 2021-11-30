736eec0078effe7fdac88bc555caa1f3659ad0d0
mbrsagor <mbrsagor@gmail.com>
2021-11-30 23:01:47 +0600

    New: Dashboard count of all dynamic show

--- app/views/dashboard_view.py
+++ app/views/dashboard_view.py
@@ -1,5 +1,21 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+from app.models.nomination import Nomination
+from app.models.user import Profile
 
 
 class DashboardView(TemplateView):
     template_name = 'dashboard/dashboard.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(DashboardView, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['nomination_list'] = Nomination.objects.all()
+        context['candidate_list'] = Nomination.objects.filter(status=True)
+        context['citizens'] = Profile.objects.all()
+        return context
